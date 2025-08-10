6a60e94d82e199ff7928e17303520d5f21f74b5e
Jim <jdmedlock@gmail.com>
2025-08-10 10:32:46 -0500

    Feature: Complete GitHub API example code

--- src/components/SelectFunction.jsx
+++ src/components/SelectFunction.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
+import '../App.css'
 import Prompt from './Prompt.jsx' // Import the Prompt component
-import PRs from './PRs.jsx'
+import PRs from './TeamInfo.jsx'
 
 const SelectFunction = () => {
   const [userSelection, setUserSelection] = useState(''); // Declare a state variable...
@@ -17,8 +18,10 @@ const SelectFunction = () => {
             Select a function to demo:
       </label>
       <br />
-      <button type="submit" name="github" onClick={handleClick}>Github API</button>
-      <button type="submit" name="prompt" onClick={handleClick}>AI Prompt</button>       
+      <div className='space-between'>
+        <button type="submit" name="github" className="button" onClick={handleClick}>Github API</button>
+        <button type="submit" name="prompt" className="button" onClick={handleClick}>AI Prompt</button>
+      </div>      
 
       {userSelection === 'github' && <PRs />}
       {userSelection === 'prompt' && <Prompt />}
